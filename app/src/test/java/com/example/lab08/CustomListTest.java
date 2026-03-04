8bbbc41c73f056a1ae8df77eabb544235ed75629
Nathan <84826494+FFlap@users.noreply.github.com>
2026-03-03 17:32:02 -0700

    nvm
    - i guess we don't need to acocunt for missing

--- app/src/test/java/com/example/lab08/CustomListTest.java
+++ app/src/test/java/com/example/lab08/CustomListTest.java
@@ -12,12 +12,4 @@ public class CustomListTest {
 
         assertTrue(list.hasCity(calgary));
     }
-
-    @Test
-    public void testHasCityReturnsFalseWhenMissing() {
-        CustomList list = new CustomList();
-        City calgary = new City("Calgary", "AB");
-
-        assertFalse(list.hasCity(calgary));
-    }
 }
